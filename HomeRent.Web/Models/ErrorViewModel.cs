6d5089db73591765c55d421c4cd2e36d76aa7143
AwakexD <aleks.parvanow@gmail.com>
2024-09-26 00:29:01 +0300

    Initialize Project

---
+++ HomeRent.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeRent.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
