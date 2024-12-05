d4430ef5cf3c8bcfe5876477e35f9a3b45a34450
AwakexD <aleks.parvanow@gmail.com>
2024-12-05 19:19:33 +0200

    Add Property/Create Action
    - Created basic HTML structure
    - Moved out dashboard side menu in PartialView

--- HomeRent.Web/Controllers/PropertyController.cs
+++ HomeRent.Web/Controllers/PropertyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HomeRent.Web.Controllers
 {
@@ -10,6 +11,12 @@ namespace HomeRent.Web.Controllers
         }
 
         public IActionResult All()
+        {
+            return this.View();
+        }
+
+        [Authorize(Roles = "Owner")]
+        public IActionResult Create()
         {
             return this.View();
         }
