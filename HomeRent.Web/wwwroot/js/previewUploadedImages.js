f7a07167d115af380f037c604905ca02d802693b
AwakexD <aleks.parvanow@gmail.com>
2024-12-21 16:39:20 +0200

    Adding image preview on upload.

---
+++ HomeRent.Web/wwwroot/js/previewUploadedImages.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const fileInput = document.querySelector(".ip-file");
+    const imgUploadBox = document.querySelector(".box-img-upload");
+
+    fileInput.addEventListener("change", function () {
+        const files = fileInput.files;
+        for (let i = 0; i < files.length; i++) {
+            const reader = new FileReader();
+            const file = files[i];
+
+            reader.onload = function (event) {
+                const imagePreview = document.createElement("div");
+                imagePreview.className = "item-upload file-delete";
+                imagePreview.innerHTML = `<img src="${event.target.result}" alt="img">`;
+                imgUploadBox.appendChild(imagePreview);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    });
+});
