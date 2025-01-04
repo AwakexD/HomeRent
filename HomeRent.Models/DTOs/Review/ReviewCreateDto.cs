06fceb47733ec2e2de4f74fe893f72c96cfc04bd
AwakexD <aleks.parvanow@gmail.com>
2025-01-04 21:32:06 +0200

    Added an rating option to the reviews.

--- HomeRent.Models/DTOs/Review/ReviewCreateDto.cs
+++ HomeRent.Models/DTOs/Review/ReviewCreateDto.cs
@@ -1,9 +1,12 @@
-﻿namespace HomeRent.Models.DTOs.Review
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeRent.Models.DTOs.Review
 {
     public class ReviewCreateDto
     {
         public string? Comment { get; set; }
 
+        [Range(1, 5)]
         public int Rating { get; set; }
 
         public DateTime DateReviewed { get; set; } = DateTime.Now;
--- HomeRent.Web/wwwroot/js/reviewPublishing.js
+++ HomeRent.Web/wwwroot/js/reviewPublishing.js
@@ -1,17 +1,39 @@
-﻿document.getElementById('review-form').addEventListener('submit', async function (e) {
-    e.preventDefault();
+﻿document.addEventListener('DOMContentLoaded', () => {
+    function ratingStarsHandler() {
+        document.getElementById('divRating').addEventListener('click', function (event) {
+            if (event.target.tagName.toLowerCase() !== 'span') return;
 
-    const antiForgeryToken = document.getElementsByName('__RequestVerificationToken')[1].value;
+            Array.prototype.forEach.call(document.querySelectorAll('.rating > .star'), function (el) {
+                el.classList.remove('rated');
+            });
 
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData.entries());
+            event.target.classList.add('rated');
+            let previousSiblings = Array.from(event.target.parentNode.children);
+            let index = previousSiblings.indexOf(event.target);
+            previousSiblings.slice(index).forEach(function (el) {
+                el.classList.add('rated');
+            });
 
-    const response = await fetch('/api/Review/Create', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-VERIFICATION-TOKEN': antiForgeryToken,
-        },
-        body: JSON.stringify(data),
+            document.getElementById('ratingValue').value = 5 - index;
+        });
+    }
+
+    ratingStarsHandler();
+    document.getElementById('review-form').addEventListener('submit', async function (e) {
+        e.preventDefault();
+
+        const antiForgeryToken = document.getElementsByName('__RequestVerificationToken')[1].value;
+
+        const formData = new FormData(this);
+        const data = Object.fromEntries(formData.entries());
+
+        const response = await fetch('/api/Review/Create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-VERIFICATION-TOKEN': antiForgeryToken,
+            },
+            body: JSON.stringify(data),
+        });
     });
-});
+})
