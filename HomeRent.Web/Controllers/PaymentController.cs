0f482f1d473bbd00c6c9d90045ae8d7a99233e65
AwakexD <a.parvanov@outlook.com>
2025-01-22 21:46:39 +0200

    Adding PaymentController and BookingOverview html and css skeleton

---
+++ HomeRent.Web/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using HomeRent.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeRent.Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IBookingService bookingService;
+
+        public PaymentController(IBookingService bookingService)
+        {
+            this.bookingService = bookingService;
+        }
+
+        public async Task<IActionResult> BookingOverview(Guid bookingId)
+        {
+            return this.View();
+        }
+    }
+}
