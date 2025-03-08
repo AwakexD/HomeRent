c02ed1d855ce80b796f0502c60eddc1ab9c6a192
AwakexD <aleks.parvanow@gmail.com>
2025-03-09 00:44:36 +0200

    Bug fix

--- HomeRent.Web/wwwroot/js/cancelBooking.js
+++ HomeRent.Web/wwwroot/js/cancelBooking.js
@@ -1,19 +1,18 @@
-﻿document.querySelector(".cancel-booking").addEventListener("click", function (event) {
-    event.preventDefault();
+﻿document.querySelectorAll(".cancel-booking").forEach(element => {
+    element.addEventListener("click", event => {
+        event.preventDefault();
 
-    const antiForgeryToken = document.querySelector("[name='__RequestVerificationToken']").value;
+        const antiForgeryToken = document.querySelector("[name='__RequestVerificationToken']").value;
+        const bookingId = element.getAttribute("data-booking-id");
 
-    const bookingId = this.getAttribute("data-booking-id");
-
-    fetch(`/api/Booking/CancelBooking?bookingId=${bookingId}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "RequestVerificationToken": antiForgeryToken,
-        }
-    })
-        .then(response => {
-            window.location.href = window.location.origin + "/Dashboard/Index";
+        fetch(`/api/Booking/CancelBooking?bookingId=${bookingId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "RequestVerificationToken": antiForgeryToken,
+            }
         })
-        .catch(error => console.error("Error:", error));
+            .then(response => window.location.href = "/Dashboard/Index")
+            .catch(error => console.error("Error:", error));
+    });
 });
